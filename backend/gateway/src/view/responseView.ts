3ff163192b2ae48bb750f639a515728b114c24a8
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-23 05:02:44 +1000

    Gateway/view response

---
+++ backend/gateway/src/view/responseView.ts
@@ -0,0 +1,8 @@
+export class ResponseView {
+  public responseId?: number;
+  public vacancyId?: number;
+  public userId?: number;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+  public status?: string;
+}
