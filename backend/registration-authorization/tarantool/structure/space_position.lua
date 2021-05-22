0a60448795011eadfcfac7a32ff28561e2f8f139
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-22 15:57:39 +1000

    RegAuth/schema position updated

--- backend/registration-authorization/tarantool/structure/space_position.lua
+++ backend/registration-authorization/tarantool/structure/space_position.lua
@@ -1,2 +1,2 @@
 \set language sql
-create table positions (position_id UNSIGNED UNIQUE PRIMARY KEY NOT NULL AUTOINCREMENT, name TEXT NOT NULL, description TEXT NOT NULL, created_at UNSIGNED NOT NULL, updated_at UNSIGNED NOT NULL, uuid TEXT NOT NULL)
+create table positions (position_id UNSIGNED UNIQUE PRIMARY KEY NOT NULL AUTOINCREMENT, name TEXT NOT NULL, description TEXT NOT NULL, created_at UNSIGNED NOT NULL, updated_at UNSIGNED NOT NULL, scope TEXT NOT NULL, uuid TEXT NOT NULL)
