f8c92388285114c3a280d4f322c8b7acbc2b5371
Ravino Doul <evgeniy@nekrasov.pw>
2021-05-22 19:53:00 +1000

    Gateway/config  server

---
+++ backend/gateway/src/config/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { cookieParserInitialization } from './cookieParser';
+import { bodyParserInitialization } from './bodyParser';
+import {routerInitialization} from './router';
+import {apolloGatewayInitialization} from './apolloGateway';
+
+
+export const server: express.Application = express();
+
+
+cookieParserInitialization(server);
+bodyParserInitialization(server);
+//routerInitialization(server);
+apolloGatewayInitialization(server);
